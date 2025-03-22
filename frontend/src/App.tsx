095e80d2e13e72de82dc0f56c40f353de112a8a6
A-007481D <labidabdelmalek@gmail.com>
2025-03-22 14:20:12 +0000

    test commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+    return (
+        <body className={"bg-blue-500 text-gray-700"}>Open the door
+            <div className={"bg-red-500"}>head</div>
+            <div className={"bg-yellow-500"}>Hallo</div>
+            <div className={"bg-green-500"}>Hola</div>
+            <div className={"bg-white"}>holli</div>
+            </body>
+    )}
+
+export default App
