8d55f8a4c02a07dfa9f081c77995297421f85c93
A-007481D <labidabdelmalek@gmail.com>
2025-04-11 17:02:17 +0100

    FEAT: Authentication : create laravel socialite controller

---
+++ backend/app/Http/Controllers/SocialiteController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+class SocialiteController extends Controller
+{
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function handleGoogleCallback()
+    {
+        $googleUser = Socialite::driver('google')->stateless()->user();
+
+        $user = User::firstOrCreate(
+            ['email' => $googleUser->getEmail()],
+            [
+                'name' => $googleUser->getName(),
+                'password' => bcrypt(uniqid()), // rand pw
+            ]
+        );
+
+        Auth::login($user);
+
+        return redirect('/discover');
+    }
+
+
+    public function redirectToFacebook()
+    {
+        return Socialite::driver('facebook')->redirect();
+    }
+
+    public function handleFacebookCallback()
+    {
+        $facebookUser = Socialite::driver('facebook')->user();
+        $user = User::updateOrCreate([
+            'email' => $facebookUser->getEmail(),
+            'provider_id' => 'facebook'
+        ],
+            [
+                'name' => $facebookUser->getName(),
+                'provider_id' => $facebookUser->getId(),
+                'provider_name' => 'facebook',
+                'password' => bcrypt(uniqid()),
+            ]);
+        Auth::login($user);
+
+        return redirect('/discover');
+    }
+}
