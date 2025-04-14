8cd496065dc37b338811a1e26b5a9c6a71206b3e
A-007481D <labidabdelmalek@gmail.com>
2025-04-14 21:13:25 +0100

    test: add mock unit testing

--- backend/tests/TestCase.php
+++ backend/tests/TestCase.php
@@ -6,5 +6,11 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    /**
+     * A basic test example.
+     */
+    public function test_that_true_is_true(): void
+    {
+        $this->assertTrue(true);
+    }
 }
