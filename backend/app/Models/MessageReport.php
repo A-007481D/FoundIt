b0f607394c1e00b9d66b8745db800d5e917db1f5
A-007481D <labidabdelmalek@gmail.com>
2025-04-23 10:15:24 +0100

    feat: create the message report model methods and relations

--- backend/app/Models/MessageReport.php
+++ backend/app/Models/MessageReport.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Message;
+use App\Models\User;
 
 class MessageReport extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'message_id',
+        'user_id',
+        'reason'
+    ];
+
+    public function message()
+    {
+        return $this->belongsTo(Message::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
