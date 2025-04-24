125caa0933bff6160cda6f19ed0e734315715b07
A-007481D <labidabdelmalek@gmail.com>
2025-04-24 19:02:35 +0100

    feat: create event for item updated

---
+++ backend/app/Events/ItemUpdated.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Item;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class ItemUpdated
+{
+    use Dispatchable, SerializesModels;
+
+    public Item $item;
+
+    public function __construct(Item $item)
+    {
+        $this->item = $item;
+    }
+}
