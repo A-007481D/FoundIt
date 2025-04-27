1f04d7141f0dd98e9f76ecfc8ab33f1e4b187510
A-007481D <labidabdelmalek@gmail.com>
2025-04-27 16:24:16 +0100

    update jwt broadcast to use the api guard for broadcast auth

--- backend/app/Http/Middleware/JwtBroadcastAuth.php
+++ backend/app/Http/Middleware/JwtBroadcastAuth.php
@@ -5,13 +5,16 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Tymon\JWTAuth\Facades\JWTAuth;
+use Illuminate\Support\Facades\Auth;
 
 class JwtBroadcastAuth
 {
     public function handle(Request $request, Closure $next)
     {
         try {
-            JWTAuth::parseToken()->authenticate();
+            $user = JWTAuth::parseToken()->authenticate();
+            Auth::shouldUse('api');
+            Auth::setUser($user);
         } catch (\Exception $e) {
             return response()->json(['message' => 'Unauthenticated.'], 401);
         }
