2185abcc35d26dd5bb64531879fa2ee2c31310dc
A-007481D <labidabdelmalek@gmail.com>
2025-04-28 21:39:09 +0100

    admin-side notifications for when a listing is reported

---
+++ backend/app/Notifications/ReportCreated.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\BroadcastMessage;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Notifications\Messages\DatabaseMessage;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Models\Report;
+
+class ReportCreated extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    protected Report $report;
+
+    public function __construct(Report $report)
+    {
+        $this->report = $report;
+    }
+
+    public function via($notifiable)
+    {
+        return ['database', 'broadcast'];
+    }
+
+    public function toDatabase($notifiable)
+    {
+        return [
+            'report_id' => $this->report->id,
+            'reportable_type' => class_basename($this->report->reportable_type),
+            'reportable_id' => $this->report->reportable_id,
+            'reason' => $this->report->reason,
+            'reporter' => [
+                'id' => $this->report->reporter->id,
+                'name' => $this->report->reporter->firstname . ' ' . $this->report->reporter->lastname,
+            ],
+        ];
+    }
+
+    public function toBroadcast($notifiable)
+    {
+        return new BroadcastMessage($this->toDatabase($notifiable));
+    }
+
+    public function broadcastOn($notifiable)
+    {
+        return new PrivateChannel('App.Models.User.' . $notifiable->id);
+    }
+}
